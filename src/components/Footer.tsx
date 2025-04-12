d3385cc271f064dd69a5d73ed18d4cdb3fbd3cbf
BRIGHT SIGHT <pramila.gadal.25@gmail.com>
2025-04-12 10:30:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <footer class="bg-secondary border-t py-4 text-center">
    
    with the text content:
    
        © 2025 CourseNotes. All rights reserved.
    
    Relevant files:
    
    And change it as follows:
    
    lock this footer at the bottom alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 export function Footer() {
   return (
-    <footer className="bg-secondary border-t py-4 text-center">
+    <footer className="bg-secondary border-t py-4 text-center absolute bottom-0 w-full">
       <div className="container">
         <p>&copy; {new Date().getFullYear()} CourseNotes. All rights reserved.</p>
       </div>
     </footer>
   );
 }
-
