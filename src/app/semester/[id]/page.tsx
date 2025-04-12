53590b37ded95be375a655d2f02ab9fda86265c2
BRIGHT SIGHT <pramila.gadal.25@gmail.com>
2025-04-12 10:49:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: A param property was accessed directly with `params.id`. `params` is now a Promise and should be unwrapped with `React.use()` before accessing properties of the underlying params object. In this version of Next.js direct access to param properties is still supported to facilitate migration but in a future version you will be required to unwrap `params` with `React.use()`.. Error source: src/app/semester/[id]/page.tsx (25:39) @ SemesterPage
    >
    >   23 |
    >   24 | export default function SemesterPage({params}: Props) {
    > > 25 |   const semesterId = React.use(params.id);
    >      |                                       ^
    >   26 |
    >   27 |   return (
    >   28 |     <div className="container py-10">
    >
    > Call Stack
    > 7
    >
    > Show 6 ignore-listed frame(s)
    > SemesterPage
    > src/app/semester/[id]/page.tsx (25:39)

--- src/app/semester/[id]/page.tsx
+++ src/app/semester/[id]/page.tsx
@@ -22,7 +22,7 @@ interface Props {
 }
 
 export default function SemesterPage({params}: Props) {
-  const semesterId = React.use(params.id);
+  const semesterId = React.use(Promise.resolve(params.id));
 
   return (
     <div className="container py-10">
